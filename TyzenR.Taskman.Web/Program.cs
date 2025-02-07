d818eafd76f7d0118702f48b5733f21b36d83aa2
tyzenr <jeanpaulva@gmail.com>
2025-02-08 02:31:17 +0530

    Exception emailing added in Program

--- TyzenR.Taskman.Web/Program.cs
+++ TyzenR.Taskman.Web/Program.cs
@@ -3,6 +3,7 @@ using TyzenR.Account;
 using TyzenR.Account.Common;
 using TyzenR.Account.Managers;
 using TyzenR.Account.ServiceClients;
+using TyzenR.Publisher.Shared;
 using TyzenR.Taskman.Entity;
 using TyzenR.Taskman.Managers;
 using TyzenR.Taskman.Web.Components;
@@ -13,62 +14,68 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); 
-IConfigurationRoot configuration;
-
-if (environmentName == "Development")
+try
 {
-    configuration = new ConfigurationBuilder()
-        .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
-        .Build();
+    var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    IConfigurationRoot configuration;
+
+    if (environmentName == "Development")
+    {
+        configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+            .Build();
+    }
+    else
+    {
+        configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.PROD.json")
+            .Build();
+    }
+
+    AppSettings appSettings = new AppSettings();
+    string publisherConnectionString = configuration.GetConnectionString("Publisher_ConnectionString");
+    string accountConnectionString = configuration.GetConnectionString("Account_ConnectionString");
+
+    builder.Services.AddDbContext<EntityContext>(options =>
+    {
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        options.UseSqlServer(publisherConnectionString);
+    }, ServiceLifetime.Transient);
+
+    builder.Services.AddDbContext<AccountContext>(options =>
+    {
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        options.UseSqlServer(accountConnectionString);
+    }, ServiceLifetime.Transient);
+
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddTransient<IAccountServiceClient, AccountServiceClient>();
+    builder.Services.AddTransient<IUserManager, UserManager>();
+    builder.Services.AddScoped<IAppInfo, AppInfo>();
+    builder.Services.AddTransient<ITaskManager, TaskManager>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    configuration = new ConfigurationBuilder()
-        .AddJsonFile($"appsettings.PROD.json")
-        .Build();
+    await SharedUtility.SendEmailToModertorAsync("Taskman.Program.Exception", ex.ToString());
 }
-
-AppSettings appSettings = new AppSettings();
-string publisherConnectionString = configuration.GetConnectionString("Publisher_ConnectionString");
-string accountConnectionString = configuration.GetConnectionString("Account_ConnectionString");
-
-builder.Services.AddDbContext<EntityContext>(options =>
-{
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-    options.UseSqlServer(publisherConnectionString);
-}, ServiceLifetime.Transient);
-
-builder.Services.AddDbContext<AccountContext>(options =>
-{
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-    options.UseSqlServer(accountConnectionString);
-}, ServiceLifetime.Transient);
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddTransient<IAccountServiceClient, AccountServiceClient>();
-builder.Services.AddTransient<IUserManager, UserManager>();
-builder.Services.AddScoped<IAppInfo, AppInfo>();
-builder.Services.AddTransient<ITaskManager, TaskManager>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
