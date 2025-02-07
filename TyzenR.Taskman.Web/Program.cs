31d1e5b9ee0087005159e5f8d9595eedd596cd58
tyzenr <jeanpaulva@gmail.com>
2025-02-08 01:32:45 +0530

    Service Injection Errro + Pages

--- TyzenR.Taskman.Web/Program.cs
+++ TyzenR.Taskman.Web/Program.cs
@@ -1,3 +1,5 @@
+using TyzenR.Account.Common;
+using TyzenR.Taskman.Managers;
 using TyzenR.Taskman.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,29 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+IConfigurationRoot configuration;
+
+if (environmentName == "Development")
+{
+    configuration = new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+        .Build();
+}
+else
+{
+    configuration = new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.PROD.json")
+        .Build();
+}
+
+AppSettings appSettings = new AppSettings();
+string publisherConnectionString = configuration.GetConnectionString("Publisher_ConnectionString");
+string accountConnectionString = configuration.GetConnectionString("Account_ConnectionString");
+
+builder.Services.AddScoped<IAppInfo, AppInfo>();
+builder.Services.AddTransient<ITaskManager, TaskManager>();
 
 app.UseHttpsRedirection();
 
