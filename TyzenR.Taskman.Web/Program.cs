472f54e9fef4f28896e3317b2e9b894cb436aee0
tyzenr <jeanpaulva@gmail.com>
2025-02-07 23:38:17 +0530

    Auth integration from Investor

--- TyzenR.Taskman.Web/Program.cs
+++ TyzenR.Taskman.Web/Program.cs
@@ -1,9 +1,91 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Radzen;
+using TyzenR.Account;
+using TyzenR.Account.Common;
+using TyzenR.Account.Managers;
+using TyzenR.Publisher.Shared;
+using TyzenR.Taskman.Entity;
+using TyzenR.Taskman.Managers;
 using TyzenR.Taskman.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+IConfigurationRoot configuration;
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
+if (environmentName == "Development")
+{
+    configuration = new ConfigurationBuilder()
+         .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+         .Build();
+}
+else
+{
+    configuration = new ConfigurationBuilder()
+         .AddJsonFile($"appsettings.PROD.json")
+         .Build();
+}
+
+try
+{
+    AppSettings appSettings = new AppSettings();
+    string publisherConnectionString = configuration.GetConnectionString("Publisher_ConnectionString");
+    string accountConnectionString = configuration.GetConnectionString("Account_ConnectionString");
+    if (publisherConnectionString == null)
+    {
+        throw new ApplicationException("Publisher_ConnectionString is not found in appsettings.json");
+    }
+    if (accountConnectionString == null)
+    {
+        throw new ApplicationException("Account_ConnectionString is not found in appsettings.json");
+    }
+
+    builder.Configuration.GetSection("AppSettings").Bind(appSettings);
+    builder.Services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+
+    builder.Services.AddDbContext<EntityContext>(options =>
+    {
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        options.UseSqlServer(publisherConnectionString);
+    }, ServiceLifetime.Transient);
+
+    builder.Services.AddDbContext<AccountContext>(options =>
+    {
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        options.UseSqlServer(accountConnectionString);
+    }, ServiceLifetime.Transient);
+
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddTyzenrAccountAuthentication(builder);
+
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+
+    builder.Services.AddMemoryCache();
+
+    builder.Services.AddScoped<IAppInfo, AppInfo>();
+    builder.Services.AddScoped<DialogService>();
+    builder.Services.AddScoped<NotificationService>();
+
+    builder.Services.AddTransient<IUserManager, UserManager>();
+
+    builder.Services.AddCors(options =>
+    {
+        var origin = appSettings.CorsOrigin;
+        options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder.WithOrigins(origin)
+                .SetIsOriginAllowedToAllowWildcardSubdomains()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+    });
+    // Add services to the container.
+    builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
@@ -25,3 +107,9 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+}
+catch (Exception ex)
+{
+    await SharedUtility.SendEmailToModertorAsync("Taskman.Program.Exception", ex.ToString());
+}
+
