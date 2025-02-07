4f095996b41a088a5d728b8d3253d08b104142a9
tyzenr <jeanpaulva@gmail.com>
2025-02-08 00:32:18 +0530

    entity updates

--- TyzenR.Taskman.Entity/TaskEntity.cs
+++ TyzenR.Taskman.Entity/TaskEntity.cs
@@ -7,12 +7,16 @@ namespace TyzenR.Taskman.Entity
         public string Title { get; set; }
         public string Description { get; set; }
 
+        public int Points { get; set; } 
+        public double Hours { get; set; }
+
         public DateTime CreatedOn { get; set; }
-        public DateTime UpdatedOn { get; set; }
 
         public Guid CreatedBy { get; set; }
+        public Guid AssignedTo { get; set; }
 
-        public Guid AssignedTo { get; set; }    
+        public DateTime UpdatedOn { get; set; }
         public Guid UpdatedBy { get; set; }
+        public string UpdatedIP { get; set; }
     }
 }
