7d79e64c8d785a979135b501ad13d3cbefea6847
tyzenr <jeanpaulva@gmail.com>
2025-02-28 05:37:46 +0530

    Table created

--- TyzenR.Taskman.Entity/ActionModel.cs
+++ TyzenR.Taskman.Entity/ActionModel.cs
@@ -5,7 +5,6 @@
         public ActionTypeEnum Type { get; set; }
         public Guid UserId { get; set; }    
         public DateTime UpdatedOn = DateTime.UtcNow;
-        public string UpdatedId { get; set; } = string.Empty;   
         public string EntityJson { get; set; } = string.Empty;    
     }
 }
