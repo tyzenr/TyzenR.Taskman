d5b73c8204fde6262ddf92099aeb5b9c23ae5b94
tyzenr <tyzenresearch@gmail.com>
2025-06-17 06:51:45 +0530

    TenantEntity

---
+++ TyzenR.Taskman.Entity/TenantEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TyzenR.EntityLibrary;
+
+namespace TyzenR.Taskman.Entity
+{
+    [Table("Tenant")]
+    public class TenantEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }   
+        public string Email { get; set; }   
+        public string MobileNumber { get; set; }
+        public string PAN { get; set; }
+        public string AadharNumber { get; set; }
+        public string Notes { get; set; }
+        public string NotesToTenant { get; set; }
+
+        public string RentReminderEmails { get; set; }
+
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
+
+
