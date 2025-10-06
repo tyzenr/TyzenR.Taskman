e40db771188edb82d34525de8f8b7f178166c10d
tyzenr <jeanpaulva@gmail.com>
2025-10-06 14:52:39 -0400

    Minutes added + Resizing columns

--- TyzenR.Taskman.Entity/Models/TimesheetTaskModel.cs
+++ TyzenR.Taskman.Entity/Models/TimesheetTaskModel.cs
@@ -3,6 +3,7 @@
     public class TimesheetTaskModel
     {
         public string Description { get; set; } = string.Empty; 
-        public int Hours { get; set; }  
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
     }
 }
