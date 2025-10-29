602bf039454f3c108d8571d5ff01dc910774a755
tyzenr <jeanpaulva@gmail.com>
2025-10-28 22:16:44 -0400

    Revert "Sorting Bug Fix"
    This reverts commit 59d52030010be2bf2180ce79e8fa0ef3fa3cf2b8.

--- TyzenR.Taskman.Managers/TaskManager.cs
+++ TyzenR.Taskman.Managers/TaskManager.cs
@@ -72,13 +72,13 @@ namespace TyzenR.Taskman.Managers
                 case "Status":
                     query = direction == SortDirectionEnum.Ascending
                         ? query.OrderBy(e => e.Status)
-                        : query.OrderByDescending(e => e.Status);
+                        : query.OrderByDescending(e => e.Description);
                     break;
 
                 case "AssignedTo":
                     query = direction == SortDirectionEnum.Ascending
                         ? query.OrderBy(e => e.AssignedTo)
-                        : query.OrderByDescending(e => e.AssignedTo);
+                        : query.OrderByDescending(e => e.Description);
                     break;
 
                 case "UpdatedOn":
